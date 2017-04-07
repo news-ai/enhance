b507dc94e5460da4e8518f7a38d35734816a884b
Abhi Agarwal <me@abhiagarwal.com>
2017-04-07 18:40:17 -0400

    switching to js

---
+++ email_js/validate.js
@@ -0,0 +1,32 @@
+'use strict';
+
+var verifier = require('email-verify');
+
+var emailOptions = {
+    'sender': 'abhi@newsai.org'
+};
+
+function verifyEmail(email) {
+    var deferred = Q.defer();
+
+    verifier.verify(email, emailOptions, function(err, info) {
+        if (err) {
+            deferred.reject(new Error(err));
+        } else {
+            deferred.resolve(info);
+        }
+    });
+
+    return deferred.promise;
+}
+
+function verifyEmails(emails) {
+    var allPromises = [];
+
+    for (var i = 0; i < emails.length; i++) {
+        var toExecute = verifyEmail(emails[i]);
+        allPromises.push(toExecute);
+    }
+
+    return Q.allSettled(allPromises);
+}
